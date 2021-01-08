792093fc2a4995c6aad2b44128a39cd3ade395e1
bigasmountain <bigasmountain1022@gmail.com>
2021-01-08 19:52:06 +0900

    Update s02-generate_inpmat.sh

--- map/src/src_param/s02-generate_inpmat.sh
+++ map/src/src_param/s02-generate_inpmat.sh
@@ -22,6 +22,8 @@ cd ..
 #######################
 # Specify input gridsize, input domain edge, input north-south order, diminfo & inpmat name
 
+DIMINFO="diminfo_test-15min_nc.txt"
+INPMAT="inpmat_test-15min_nc.bin"
 #--------------------
 # parameter for sample "test_15min_nc" runoff data
 GRSIZEIN=0.25       # input grid size
@@ -33,6 +35,22 @@ SOUTHIN=-60.0
 OLAT="NtoS"
 #--------------------
 
+######################
+### below is the example for sample test3-japan simulation in 
+#DIMINFO="diminfo_test-tej.txt"
+#INPMAT="inpmat_test-tej.bin"
+##--------------------
+## parameter for sample "test_15min_nc" runoff data
+#GRSIZEIN=0.0166666666666666666666       # input grid size
+#WESTIN=123.0       # input domain west east north south edge
+#EASTIN=148.0
+#NORTHIN=46.0
+#SOUTHIN=24.0
+##OLAT="StoN"        # north-south order of input data
+#OLAT="NtoS"
+##--------------------
+
+
 TAG="1min"          # tag for hires data dir (1min / 15sec / 3sec)
 if [ -f ./1min/location.txt ]; then
   TAG="1min"
@@ -40,13 +58,13 @@ elif [ -f ./15sec/location.txt ]; then
   TAG="15sec"
 fi
 
-DIMINFO="diminfo_test-15min_nc.txt"
-INPMAT="inpmat_test-15min_nc.bin"
-
 #######################
 # generate inpmat
 #   Fortran code is for linear Cartesian input
 #   for other grid coordinate, code should be rewritten
 
 ./src_param/generate_inpmat $TAG $GRSIZEIN $WESTIN $EASTIN $NORTHIN $SOUTHIN $OLAT $DIMINFO $INPMAT
+
+
+
 
