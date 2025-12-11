0b235b20b32ba400298abcaf38adda4e437d3a72
Dai Yamazaki <bigasmountain1022@gmail.com>
2025-12-11 18:25:31 +0900

    small files

---
+++ etc/levee_params/auto.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#s00-copy_data.sh
+#s01-calc_levee_fraction.sh
+#s02-conv_FLOPROS_res.sh
+s03-cycle_sim00.sh
+s04-cycle_sim01.sh
+s05-run_levee.sh
+s06-downscale.sh
+x01-calc_levbaseheight.sh
